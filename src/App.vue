122577e8f6cb3af82e1df76d5ba5872f2ce60099
Ajuka David <ajuka.zephiniah@gmail.com>
2022-03-05 10:16:35 +0100

    used composition API with ts

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+<div class="app">
+ <p>
+   {{jobs[0].location}}
+ </p>
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref } from 'vue';
+import Job from './types/Job'
+
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  setup(){
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 25 as string | number
+    // })
+    // state.name = 'shaun'
+    // return {
+    //   ...toRefs(state)
+    // }
+
+  //   const name = ref('Link')
+  //   const age = ref<number | string>(12)
+
+
+  //  return {
+  //    name, age
+
+  //  }
+
+  const jobs = ref<Job[]>([ //this is an array of Job objects
+      {
+        title: 'Farm worker',
+        location: 'lon lon ranch',
+        salary: 30000,
+        id: '1'
+      },
+      {
+        title: 'quarryman',
+        location: 'Death mountain',
+        salary: 40000,
+        id: '2'
+      },
+      {
+        title: 'flute player',
+        location: 'the lost woods',
+        salary: 35000,
+        id: '3'
+      },
+      {
+        title: 'fisherman',
+        location: 'lake hylia',
+        salary: 21000,
+        id: '4'
+      },
+      {
+        title: 'prison guard',
+        location: 'gerudo valley',
+        salary: 32000,
+        id: '1'
+      }
+  ])
+
+  return {
+    jobs
   }
+  },
+
+
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
